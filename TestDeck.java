da41b3f4873a72a001547db91f2952944d02ee48
ShaneMcNiff <shanemcniff@gmail.com>
2015-03-29 22:28:43 +0100

    Create TestDeck.java

---
+++ TestDeck.java
@@ -0,0 +1,7 @@
+public class TestDeck {
+	public static void main(String[]args){
+		System.out.print(newDeck.toString());
+		System.out.print("\n\n");
+		System.out.print(newDeck.getNextCard(51));
+	}
+}
