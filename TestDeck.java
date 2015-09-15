d70e91a7e8e1b12a5a9c8e0ef4b4f56fc96b501c
ShaneMcNiff <shanemcniff@gmail.com>
2015-09-15 13:22:41 +0100

    Delete TestDeck.java

--- TestDeck.java
+++
@@ -1,7 +0,0 @@
-public class TestDeck {
-	public static void main(String[]args){
-		System.out.print(newDeck.toString());
-		System.out.print("\n\n");
-		System.out.print(newDeck.getNextCard(51));
-	}
-}
